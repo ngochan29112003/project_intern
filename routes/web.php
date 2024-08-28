23ecc681c73fb71de586e3ec4254530dcf0fc4c9
フィン　アィン <145379564+fuinain@users.noreply.github.com>
2024-08-29 01:43:57 +0700

    1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\BhxhController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DepartmentController;
